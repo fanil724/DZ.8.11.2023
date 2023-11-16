b757adce8a11ad021cb7fb38374628a082cdda96
Фаниль <fanil724@gmail.com>
2023-11-16 18:41:49 +0300

    done  DZ 15.11.2023

--- 13.11.2023/ApplicationContext.cs
+++ 13.11.2023/ApplicationContext.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
+using Microsoft.VisualBasic;
 
 namespace _13._11._2023
 {
@@ -8,13 +10,39 @@ namespace _13._11._2023
         public DbSet<Customer> customers { get; set; }
         public ApplicationContext(string connect) : base(CreatOPTIONS(connect))
         {
-           // Database.EnsureDeleted();
+            //Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer();
+            optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Trace);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Debug);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Warning);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Error);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Critical);
+            //optionsBuilder.LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.None);
+
+
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { SqliteEventId.PrimaryKeyFound });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { CoreEventId.QueryCompilationStarting });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { RelationalEventId.CommandExecuted });
+
+
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Model.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Query.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Infrastructure.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Scaffolding.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Update.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Migrations.Name });
+
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Connection.Name });
+            //optionsBuilder.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Transaction.Name });
+
+
         }
 
         private static DbContextOptions<ApplicationContext> CreatOPTIONS(string nameConnect)
@@ -27,7 +55,6 @@ namespace _13._11._2023
 
             var optionBilder = new DbContextOptionsBuilder<ApplicationContext>();
             var options = optionBilder.UseSqlServer(connectionString).Options;
-
             return options;
         }
 
--- 13.11.2023/Program.cs
+++ 13.11.2023/Program.cs
@@ -2,18 +2,18 @@
 
 using (ApplicationContext db = new ApplicationContext("DefaultConnection"))
 {
-    Customer c1 = new Customer() { Age = 33, FirstName = "Petr", LastName = "Petrov", Address = "Lenina 89", Email = "pter@mail.ru", Phone = "8907567523" };
-    Customer c2 = new Customer() { Age = 22, FirstName = "Sema", LastName = "Sidorov", Address = "Pushkina 29", Email = "sem@mail.ru", Phone = "8907567523" };
-    Customer c3 = new Customer() { Age = 21, FirstName = "Volodya", LastName = "Ivanov", Address = "Ostrovckovo 9", Email = "vovo@mail.ru", Phone = "8902134132" };
-    Customer c4 = new Customer() { Age = 23, FirstName = "Ivan", LastName = "Petrov", Address = "Lenina 39", Email = "ivann@mail.ru", Phone = "8976345423" };
-    Customer c5 = new Customer() { Age = 42, FirstName = "Dima", LastName = "Puprin", Address = "Chulman 89", Email = "pter@mail.ru", Phone = "8923545423" };
+    //Customer c1 = new Customer() { Age = 33, FirstName = "Petr", LastName = "Petrov", Address = "Lenina 89", Email = "pter@mail.ru", Phone = "8907567523" };
+    //Customer c2 = new Customer() { Age = 22, FirstName = "Sema", LastName = "Sidorov", Address = "Pushkina 29", Email = "sem@mail.ru", Phone = "8907567523" };
+    //Customer c3 = new Customer() { Age = 21, FirstName = "Volodya", LastName = "Ivanov", Address = "Ostrovckovo 9", Email = "vovo@mail.ru", Phone = "8902134132" };
+    //Customer c4 = new Customer() { Age = 23, FirstName = "Ivan", LastName = "Petrov", Address = "Lenina 39", Email = "ivann@mail.ru", Phone = "8976345423" };
+    //Customer c5 = new Customer() { Age = 42, FirstName = "Dima", LastName = "Puprin", Address = "Chulman 89", Email = "pter@mail.ru", Phone = "8923545423" };
 
-    db.Add(c1);
-    db.Add(c2);
-    db.Add(c3);
-    db.Add(c4);
-    db.Add(c5);
-    db.SaveChanges();
+    //db.Add(c1);
+    //db.Add(c2);
+    //db.Add(c3);
+    //db.Add(c4);
+    //db.Add(c5);
+    //db.SaveChanges();
 
     var cus = db.customers.ToList();
     foreach (var c in cus)
@@ -23,26 +23,26 @@ using (ApplicationContext db = new ApplicationContext("DefaultConnection"))
 }
 Console.WriteLine();
 
-using (ApplicationContext db = new ApplicationContext("MSSQLSERVERConnection"))
-{
-    Customer c1 = new Customer() { Age = 33, FirstName = "Petr", LastName = "Petrov", Address = "Lenina 89", Email = "pter@mail.ru", Phone = "8907567523" };
-    Customer c2 = new Customer() { Age = 22, FirstName = "Sema", LastName = "Sidorov", Address = "Pushkina 29", Email = "sem@mail.ru", Phone = "8907567523" };
-    Customer c3 = new Customer() { Age = 21, FirstName = "Volodya", LastName = "Ivanov", Address = "Ostrovckovo 9", Email = "vovo@mail.ru", Phone = "8902134132" };
-    Customer c4 = new Customer() { Age = 23, FirstName = "Ivan", LastName = "Petrov", Address = "Lenina 39", Email = "ivann@mail.ru", Phone = "8976345423" };
-    Customer c5 = new Customer() { Age = 42, FirstName = "Dima", LastName = "Puprin", Address = "Chulman 89", Email = "pter@mail.ru", Phone = "8923545423" };
+//using (ApplicationContext db = new ApplicationContext("MSSQLSERVERConnection"))
+//{
+//    Customer c1 = new Customer() { Age = 33, FirstName = "Petr", LastName = "Petrov", Address = "Lenina 89", Email = "pter@mail.ru", Phone = "8907567523" };
+//    Customer c2 = new Customer() { Age = 22, FirstName = "Sema", LastName = "Sidorov", Address = "Pushkina 29", Email = "sem@mail.ru", Phone = "8907567523" };
+//    Customer c3 = new Customer() { Age = 21, FirstName = "Volodya", LastName = "Ivanov", Address = "Ostrovckovo 9", Email = "vovo@mail.ru", Phone = "8902134132" };
+//    Customer c4 = new Customer() { Age = 23, FirstName = "Ivan", LastName = "Petrov", Address = "Lenina 39", Email = "ivann@mail.ru", Phone = "8976345423" };
+//    Customer c5 = new Customer() { Age = 42, FirstName = "Dima", LastName = "Puprin", Address = "Chulman 89", Email = "pter@mail.ru", Phone = "8923545423" };
 
-    db.Add(c1);
-    db.Add(c2);
-    db.Add(c3);
-    db.Add(c4);
-    db.Add(c5);
-    db.SaveChanges();
+//    db.Add(c1);
+//    db.Add(c2);
+//    db.Add(c3);
+//    db.Add(c4);
+//    db.Add(c5);
+//    db.SaveChanges();
 
-    var cus = db.customers.ToList();
-    foreach (var c in cus)
-    {
-        c.print();
-    }
-}
+//    var cus = db.customers.ToList();
+//    foreach (var c in cus)
+//    {
+//        c.print();
+//    }
+//}
 
 
